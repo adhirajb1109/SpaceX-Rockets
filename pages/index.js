2ef75f86d4f46438ee49e6941983bfb5c905b592
Adhiraj Bhatia <adhirajb1109@gmail.com>
2021-09-02 11:07:02 +0530

    Commit

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home({ launches }) {
       <Head>
         <title>Spacex Rockets</title>
         <meta name="description" content="Spacex Rockets is an app which tells us about all the missions of Spacex ." />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous"></link>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"></link>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"></script>
       </Head>
 
       <main>
@@ -17,7 +17,7 @@ export default function Home({ launches }) {
           {launches.map((launch) => (
             <div className="card mx-4 my-4" key={launch.id}>
               <div className="card-body">
-                <h5><a className="card-title" href={launch.links.wikipedia} className="text-decoration-none text-dark">{launch.mission_name}</a></h5>
+                <h5><a className="card-title text-decoration-none text-dark" href={launch.links.wikipedia}>{launch.mission_name}</a></h5>
                 <p className="card-text mt-3">Rocket &rarr; {launch.rocket.rocket_name}</p>
                 <p className="card-text">Launch Year &rarr; {launch.launch_year}</p>
               </div>
