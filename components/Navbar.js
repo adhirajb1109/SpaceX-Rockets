b7dbbbbb710e50118689e17b6c0e8e4e4828052e
Adhiraj Bhatia <adhirajb1109@gmail.com>
2021-09-02 09:50:11 +0530

    Initial Commit

---
+++ components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+                <div className="container-fluid">
+                    <Link href="/" passHref><a className="navbar-brand">Spacex Rockets</a></Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link aria-current="page" href="/" passHref><a className="nav-link active">Home</a></Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
